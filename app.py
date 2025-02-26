bbdf59a1931bef432bd3a33822716bb25d1da218
dushyant4342 <56916136+dushyant4342@users.noreply.github.com>
2025-02-26 12:24:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
 # AWS Bedrock Client
-bedrock = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
+bedrock = boto3.client(service_name="bedrock-runtime", region_name="ap-south-1")
 bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v2:0", client=bedrock)
 
 # Paths - Inside Docker container
